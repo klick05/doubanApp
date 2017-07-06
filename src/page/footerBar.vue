4533543a40e227e41d1d75390f9ec4a21bb2dceb
xiao ke <188683784@qq.com>
2017-07-06 17:59:59 +0800

    移动端自适应布局

---
+++ src/page/footerBar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="footerBar">
+      <div class="haha1">132132</div>
+  </div>
+</template>
+
+<script>
+  
+</script>
+
+<style lang="scss">
+@import '../style/mixin.scss';
+.haha1{
+  background:red;
+  @include wh(px2rem(250),px2rem(250))
+}
+</style>
